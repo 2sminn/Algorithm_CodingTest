e7758e3daa92b41b08397b44aa09315df2f4f938
2sminn <sungmingod13@gmail.com>
2025-12-30 00:40:12 +0900

    [Gold III] Title: 나머지 합, Time: 504 ms, Memory: 123288 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,25 +1,29 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        long[] sum = new long[N];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        long[] sum = new long[N + 1];
         long[] count = new long[M];
         long answer = 0;
-        sum[0] = sc.nextInt();
-        for(int i = 1; i < N; i++) {
-            sum[i] = sum[i - 1] + sc.nextInt();
-        }
-        for(int i = 0; i < N; i++){
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++){
+            while(!st.hasMoreTokens()){
+                st = new StringTokenizer(br.readLine());
+            }
+            sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
             int remainder = (int) (sum[i] % M);
             if(remainder == 0) answer++;
             count[remainder]++;
         }
         for(int i = 0; i < M; i++){
-            if(count[i] > 1){
+            if(count[i] > 1) {
                 answer += count[i] * (count[i] - 1) / 2;
             }
         }
